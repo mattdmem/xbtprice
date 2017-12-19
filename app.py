b78f9850e723bf1a067e0ed2ced9c10040313de8
mattdparker <matthewdanielparker@gmail.com>
2017-12-19 17:47:42 +0000

    1st commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask
+from flask_restful import Resource, Api
+import re
+
+app = Flask(__name__)
+api = Api(app)
+
+class Price(Resource):
+    def get(self):
+
+        result = requests.get('http://www.hl.co.uk/shares/shares-search-results/x/xbt-provider-ab-bitcoin-tracker-one',verify=False).content
+        search = re.search('<span class="bid price-divide" >(.*)</span>', result)
+
+
+        btc = search.group(1).replace(" SEK","")
+
+
+        result = requests.get('http://www.hl.co.uk/shares/shares-search-results/x/xbt-provider-ab-ethereum-tracker-eur',verify=False).content
+        search = re.search('<span class="bid price-divide" >(.*)</span>', result)
+
+
+        eth = search.group(1).replace("&euro;","")
+
+        data = { 'data' : { 'eth':eth, 'btc':btc } }
+
+        return data
+
+api.add_resource(Price, '/price')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5004)
